5f49e00f1298c1b7179f6c19ec8df3e334176367
Tsukuru <tsukuru501@gmail.com>
2025-02-27 14:35:29 +0900

    人物の移動以外良い感じ

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/first-success-model.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
